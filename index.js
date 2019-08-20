f9ec681a052a6884786c751d2aa38777412977c6
Ramarduk <ramarduk@gmail.com>
2019-08-20 12:50:28 -0600

    Works

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
 'use strict';
 
 const express = require('express');
-const playstore = require('./Playstore.js')
+const morgan = require('morgan');
+
+const playstore = require('./playstore');
+
 const app = express();
 
+app.use(morgan('dev'));
+
 app.get('/apps', (req, res) => {
-  res.json(playstore);
+
+  let {sort, genres} = req.query;
+
+  let apps = [...playstore];
+
+  if (genres) {
+    const validGenres = ['action', 'puzzle', 'strategy', 'casual', 'arcade', 'card'];
+    if (!validGenres.includes(genres.toLowerCase())) {
+      return res
+        .status(400)
+        .send('Genre must be one of the following: "Action", "Puzzle", "Strategy", "Casual", "Arcade", "Card"');
+    }
+    apps = apps.filter(app => app.Genres.toLowerCase().includes(genres.toLowerCase()));
+  }
+  if (sort) {
+    if (!(sort.toLowerCase() === 'rating') && !(sort.toLowerCase() === 'app')){
+      return res
+        .status(400)
+        .send('Must sort by "app" or "rating"');
+    }
+    if (sort.toLowerCase() === 'rating'){
+      apps = apps.sort((app1, app2) => {
+        return app2.Rating - app1.Rating;
+      });
+    }
+    if (sort.toLowerCase() === 'app'){
+      apps = apps.sort((app1, app2) => {
+        if (app2.App.toLowerCase() > app1.App.toLowerCase())
+          return -1;
+        else 
+          return 1;
+      });
+    }
+  }
+
+  return res.json(apps);
+
 });
-  
+
 app.listen(8000, () => {
-  console.log('Express server is listening on port 8000!');
+  console.log('Server on 8000!');
 });
